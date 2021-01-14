8bf50056e6566d135d08ca8ec200b87efbac1c24
Blezyl Santos <blezylsantos@yahoo.com>
2021-01-14 15:42:31 -0500

    Added console file

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+//Turn in error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once('vendor/autoload.php');
+// create an instance of the Base class
+$f3 = Base::instance();
+$f3-> set('DEBUG', 3);
+
+// define a default route (home page)
+$f3 -> route ('GET /', function(){
+    $view = new Template();
+    echo $view->render("views/home.html");
+});
+
+// run fat free
+$f3->run();
